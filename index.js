7b5420578a8d6ca9295db9f0aec231b57af58dd1
+ <+>
2022-11-27 12:54:20 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+  
+    const names = ["Ada","Brendan","Ali"]
+    const eventName = ["surprise"]
+    function writeCards(names ,eventName) {
+      let array=[]
+      for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    
+      }
+          return array;
+    } 
+    
+
+      function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+      }
+    }
+    
